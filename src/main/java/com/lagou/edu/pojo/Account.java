9b6b35fc8256552fb935ed8b8387fb868933e1e7
AIRSOLA <wzw197749957@126.com>
2020-07-24 17:00:37 +0800

    can boot

--- src/main/java/com/lagou/edu/pojo/Account.java
+++ src/main/java/com/lagou/edu/pojo/Account.java
@@ -5,7 +5,24 @@ public class Account {
     private String name;
     private int money;
     private String cardNo;
+    private String userName;
+    private String passWord;
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 
     public String getName() {
         return name;
